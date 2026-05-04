a246fca06d5ad39156b940cacfb2da25f57fd646
makson <94912293+Maksymkek@users.noreply.github.com>
2026-05-04 16:51:43 +0300

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Максим Грищенков on 04.05.2026.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct WeatherNotesApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
